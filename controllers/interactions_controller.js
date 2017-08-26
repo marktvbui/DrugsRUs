c0240559e3e4f010e14ec02ee9c6738a3a6248c8
Mark Bui <tvyybui@yahoo.com>
2017-08-26 11:07:00 -0700

    working with interations still

--- controllers/interactions_controller.js
+++ controllers/interactions_controller.js
@@ -2,8 +2,12 @@ var db = require('../models');
 
 module.exports = function(app) {
   app.get('/interactions', function(req, res) {
-    var med1 = 'Monopril';
-    var med2 = 'Klorvess';
+    // var med1 = req.query.med1_name;
+    // var med2 = req.query.med2_name;
+    // console.log(req.query.med1_name);
+    // console.log(req.query.med2_name);
+    var med1 = 'MONOPRIL';
+    var med2 = 'KLORVESS';
     console.log('interactions controller hit');
     db.Interactions.findAll({
       where : {
@@ -11,17 +15,22 @@ module.exports = function(app) {
         med2_name: med2
       }
     }).then(function(results) {
-      console.log('++++++++++++++++++++++++');
-      console.log(results[0].dataValues.interaction);
-      // return interactionsResult;
-      var interactionsResult;
-      var interactions = {
-        interactionsResult: results
+      // console.log(results[0].dataValues.interaction);
+    //   if (results.length === 0){
+    //     res.send('no interactions found')
+    //   } else {
+    //   var interactions = {
+    //     interactionsResult: results[0].dataValues.interaction
+    //   };
+    //   console.log('------------------------');
+    //   res.send(interactions);
+    // }
+    var interactions = {
+        interactionsResult: results[0].dataValues.interaction
       };
       console.log('------------------------');
       console.log(interactions);
-
-        res.render('index', interactions);
+      res.send(interactions);
     });
   });
 };
