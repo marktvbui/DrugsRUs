50403d1114bf89230db86bffb731d70b17787d77
Mark Bui <tvyybui@yahoo.com>
2017-08-28 17:50:36 -0700

    dried up drugs.js, working on user page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -29,15 +29,9 @@ module.exports = function(app) {
     db.User.findAll({
       include: {
         model: db.Medicine
-      },
-      include: {
-        model: db.Interactions
       }
     }).then(function(data){
-      var userObj = {
-        User: data
-      };
-        res.render('user', userObj);
+        res.send(data);
       // });
     });
   });
--- public/assets/js/drugs.js
+++ public/assets/js/drugs.js
@@ -17,7 +17,6 @@ $('#drug-submit').on("click", function(event){
       var Indication = response.results[0].indications_and_usage[0];
       searchArray.push({'brand': brand, 'generic': generic, 'Indication': Indication});
       displayInfo();
-      $("#drug").val('');
   });
     $("#drugChoice").show();
 
@@ -32,10 +31,8 @@ $('#drugChoice').on("click", function(event){
   $.ajax({
     // data: {data:"med1_name=" + searchArray[0].brand + "med2_name=" + searchArray[1].brand},
     url: '/interactions?med1_name=' + searchArray[last].brand + "&med2_name=" + searchArray[next].brand,
-     // url: '/interactions',
     method: "GET",
     cache: false,
-    // async: true,
     error : function(request,error)
     {
       alert("Request: "+JSON.stringify(request));
@@ -43,31 +40,23 @@ $('#drugChoice').on("click", function(event){
   }).done(function(resp){
     console.log(resp);
     var divInteractions = $('<div class="drugInteractions container ">');
-    divInteractions.html("<h3>Interactions: "+ resp + "</h3");
+    divInteractions.html("<h3 class='drugInteracts'>Interactions: "+ resp + "</h3");
     divInteractions.append('</div>');
     $('.InteractionsResults').append(divInteractions);
   });
 });
 
 function displayInfo() {
-  $('.test1').empty();
+  $('.drugAPIResults').empty();
 
-  var last = searchArray.length - 1;
-  var next = searchArray.length -2;
+  for (i = searchArray.length -1; i > searchArray.length -3; i--) {
+    var div1 = $('<div class="test2 col-md-6">');
+    div1.append("<h2 class='drugBrandName'>Brand Name: " + searchArray[i].brand + "</h2>");
+    div1.append("<h4 class='drugGenericName'>Generic Name: " + searchArray[i].generic + "</h4>");
+    div1.append("<h4 class='drugIndications'>Indications: " + searchArray[i].Indication + "</h4>");
+    div1.append($('</div>'));
+    $("#drug").val('');
+    $('.drugAPIResults').append(div1);
+  }
 
-  var div1 = $('<div class="test2 col-md-6">');
-  div1.append("<h2>Brand Name: " + searchArray[last].brand + "</h2>");
-  div1.append("<h4>Generic Name: " + searchArray[last].generic + "</h4>");
-  div1.append("<h4>Indications: " + searchArray[last].Indication + "</h4>");
-  div1.append($('</div>'));
-  $("#drug").val('');
-  $('.test1').append(div1);
-
-  var div1 = $('<div class="test2 col-md-6">');
-  div1.append("<h2>Brand Name: " + searchArray[next].brand + "</h2>");
-  div1.append("<h4>Generic Name: " + searchArray[next].generic + "</h4>");
-  div1.append("<h4>Indications: " + searchArray[next].Indication + "</h4>");
-  div1.append($('</div>'));
-  $('.test1').append(div1);
-  $("#drug").val('');
 };
